754ec9977622df9f513772e2039fa9eeb5cd16eb
David Sadler <davidwallacesadler@gmail.com>
2025-11-30 12:04:39 -0700

    Get initial patterns going, Kayla start OOPing
    Co-authored-by: Kayla Sudweeks <kaySuds@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 #include <string>
 
+// Encapsulates a single action in the story 
+struct StoryAction {
+    // display text for the action
+    std::string display_text;
+
+    // the number that must be input for the action to take place
+    std::string number;
+
+    // identifies the next beat in the story via an id 
+    std::string next_beat_id;
+};
+
+// Encapsulates a single beat in the story
+struct StoryBeat {
+    // identifies this particular story beat, used to reference beats from actions
+    std::string id;
+
+    // image associated with story beat to be displayed at beginning of the beat
+    std::string ascii_image;
+
+    // main narrative for beat, to be displayed
+    std::string narrative_block;
+    
+    // array of actions for the beat
+    StoryAction actions[]; 
+};
+
+StoryBeat story_beats[]{
+    StoryBeat{} // TODO: we need to create constructors (think swift init) for StoryBeat & StoryAction
+    // TODO: we need to turn our current text in main into story beats inside array
+};
+
 int main() 
 {
     std::cout << "------------\n";
@@ -59,8 +91,85 @@ int main()
     // c. Get user input
     // d. based on that go down different narrative path
 
-    // NOTE: Have ascci art intro image to this character's story
-    // std::string ascii = R"()";
+    // a. Print ascii image for the selected character (Mark)
+    std::string ascii = R"(
+        .:::::::.
+      .'     ':::
+      :        ::.
+      :-  --   ' :
+      :        ..:
+      :.--    .'::;
+      ::.__ .'  ':;_
+      ::/""".    .' ""-._
+      ::\   |   :        :
+     .:::  .'..'          '
+    . :'  .         '     ':
+    : '  .:        .     . .
+    :'   .:        :    .:  "--__
+   /'   .::        :   .
+  _: . ::::        '   .
+.'    '-----------:   .
+ :                    '---''--'--
+    )";
+
+    std::cout << ascii;
+    std::cout << "\n";
+
+    // b. Initial text for Mark
+    std::cout << "I sit down on the hard chair in the waiting room, out of breath and lean my cane against the wall behind me.";
+    std::cout << "\n";
+
+    std::string mark_one = R"(
+Across from me sits a woman, about my age, young, early-thirties, who hasn't looked up from her phone. 
+Behind her are floor toceiling windows.
+I start to glance around the room. 
+A frost-tinted glass door reads NOT AN EXIT, a fire extinguisher hangs on a wood paneled wall.
+I wonder where the door leads, whether or not it's an emergency exit. 
+Aren't those marked EMERGENCY EXIT?
+)"; 
+    std::cout << mark_one;
+    std::cout << "\n";
+
+    // c. Define and present actions
+
+    std::cout << "What do you do?\n";
+
+    std::cout << "\n";
+
+    std::cout << "1 - Check phone\n";
+    std::cout << "2 - Look at ceiling\n";
+    std::cout << "3 - Listen to the receiptionists\n";
+
+    std::cout << "\n";
+    std::cout << "Enter the number: ";
+
+    std::string action_selection;
+    std::cin >> action_selection;
+
+    bool is_action_valid = false;
+    for (int i = 0; i < 3; i++) {
+        std::string selection = valid_selections[i];
+        if (action_selection == selection) {
+            is_action_valid = true;
+            break;
+        }
+    }
+    if (!is_action_valid) {
+        std::cout << "YOU MESSED UP\n";
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "\n";
+    std::cout << "You've selected: ";
+    if (action_selection == "1") {
+        std::cout << "Check phone\n";
+    } else if (action_selection == "2") {
+        std::cout << "Look at ceiling\n";
+    } else {
+        std::cout << "Listen to the receiptionists\n";
+    }
+
+    
 
     return EXIT_SUCCESS;
 }
